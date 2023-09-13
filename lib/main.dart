ae5de5172970ddae8ed25338df1a4bbc70f9e81d
seoziyeong <bestzenio@gmail.com>
2023-09-13 22:23:09 +0900

    feat : widget life sycle

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nomadcoder_flutter_webtoon/widgets/button.dart';
 
 void main() {
   runApp(const App());
@@ -12,6 +13,15 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
+  //
   // 상태변경할 데이터 준비, final 붙이지 않음
   int counter = 0;
 
@@ -35,12 +45,24 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.red,
+          ),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              showTitle ? const MyLargeTitle() : const Text("nothing"),
+              TextButton(
+                onPressed: toggleTitle,
+                child: const Text("TOGGLE TITLE"),
+              ),
               const Text(
                 "click count",
                 style: TextStyle(
@@ -74,6 +96,47 @@ class _AppState extends State<App> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  // * widget life sycle
+  // * initState > build > dispose
+
+  // initState : 상태 초기화 메소드, build 전 실행됨
+  // 부모 요소에 의존하는 데이터를 초기화해야 하는 경우
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // dispose : 위젯이 스크린에서 제거될 때 호출되는 메서드
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  // 화면에 ui 그려지는 단계
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Theme 상속받은 Title",
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
       ),
     );
   }
