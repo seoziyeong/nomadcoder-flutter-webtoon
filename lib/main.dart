640f5077492ea93db1ec23c904ec9805683c9af9
seoziyeong <bestzenio@gmail.com>
2023-09-13 20:43:16 +0900

    feat : counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,147 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:nomadcoder_flutter_webtoon/widgets/button.dart';
-import 'package:nomadcoder_flutter_webtoon/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
 }
 
-// StatelessWidget을 상속받아 위젯화
-// 모든 앱의 기본 UI 설정: CupertinoApp or MaterialApp 중에 선택 (apple or google)
-// 모든 화면은 Scaffold(구조)를 가져야 함
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // 상태변경할 데이터 준비, final 붙이지 않음
+  int counter = 0;
+
+  // 함수 작성, setState로 상태변경 로직 작성
+  // setState 안에 코드를 안써도 반영 되지만, 되도록 안에 작성
+  void onClicked() {
+    setState(() {
+      counter = counter + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          // 세로 스크롤
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "click count",
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text("Hey, Berry 🍓",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800,
-                            )),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+              ),
+              Text(
+                "$counter", // * 데이터
+                style: const TextStyle(
+                  fontSize: 30,
                 ),
-                const SizedBox(
-                  height: 64,
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked, // * trigger
+                icon: const Icon(
+                  Icons.add_box_rounded,
                 ),
-                Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white.withOpacity(0.7),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                const Text(
-                  "\$5 184 482",
-                  style: TextStyle(
-                      fontSize: 48,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xFFF2B33A),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: 'Request',
-                      bgColor: Color(0xfff1f2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Wallets",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      "View All",
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.7),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                const CurrencyCard(
-                  name: "Euro",
-                  code: "EUR",
-                  amount: "6 428",
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                  offsetOrder: 0,
-                ),
-                const CurrencyCard(
-                  name: "Dollar",
-                  code: "USD",
-                  amount: "55 622",
-                  icon: Icons.attach_money_rounded,
-                  isInverted: true,
-                  offsetOrder: -20,
-                ),
-                const CurrencyCard(
-                  name: "Rupee",
-                  code: "INR",
-                  amount: "28 981",
-                  icon: Icons.currency_rupee_rounded,
-                  isInverted: false,
-                  offsetOrder: -40,
-                ),
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
