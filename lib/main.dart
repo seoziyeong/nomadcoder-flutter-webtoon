cc9c534854db1a526c38fcf47f0ec7f0840f704b
seoziyeong <bestzenio@gmail.com>
2023-09-13 20:59:33 +0900

    feat : list countup

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,19 @@ class _AppState extends State<App> {
   int counter = 0;
 
   // 함수 작성, setState로 상태변경 로직 작성
-  // setState 안에 코드를 안써도 반영 되지만, 되도록 안에 작성
+  // setState가 있어야 화면에 상태변경된 것이 반영됨
+  // setState 안에 코드를 안써도 반영 되지만, 가독성을 위해 되도록 안에 작성
   void onClicked() {
     setState(() {
       counter = counter + 1;
+    });
+  }
+
+  List<int> numbers = [];
+
+  void onClicked2() {
+    setState(() {
+      numbers.add(numbers.length);
     });
   }
 
@@ -50,7 +59,18 @@ class _AppState extends State<App> {
                 icon: const Icon(
                   Icons.add_box_rounded,
                 ),
-              )
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              for (var n in numbers) Text("$n"),
+              IconButton(
+                iconSize: 40,
+                onPressed: onClicked2, // * trigger
+                icon: const Icon(
+                  Icons.add_box_rounded,
+                ),
+              ),
             ],
           ),
         ),
