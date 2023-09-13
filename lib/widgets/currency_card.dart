2746f4d15aea4382bb1e75ca64755fea26ae4c9b
seoziyeong <bestzenio@gmail.com>
2023-09-13 17:46:34 +0900

    design : card ui

---
+++ lib/widgets/currency_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class CurrencyCard extends StatelessWidget {
+  final String name, code, amount;
+  final IconData icon;
+  final bool isInverted;
+
+  final _blackColor = const Color(0xFF1F2123);
+
+  const CurrencyCard({
+    super.key,
+    required this.name,
+    required this.code,
+    required this.amount,
+    required this.icon,
+    required this.isInverted,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      clipBehavior: Clip.hardEdge, // overflow: hidden;
+      decoration: BoxDecoration(
+          color: isInverted ? Colors.white : _blackColor,
+          borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: isInverted ? _blackColor : Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      amount,
+                      style: TextStyle(
+                        color: isInverted ? _blackColor : Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      code,
+                      style: TextStyle(
+                        color: isInverted
+                            ? _blackColor
+                            : Colors.white.withOpacity(0.7),
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Transform.scale(
+              scale: 2,
+              child: Transform.translate(
+                offset: const Offset(-5, 12),
+                child: Icon(
+                  icon,
+                  color: isInverted ? _blackColor : Colors.white,
+                  size: 96,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
