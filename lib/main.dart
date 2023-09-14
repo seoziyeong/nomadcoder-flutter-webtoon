91ad8c45aad7244a889ffc586d22ad97f7d48e88
seoziyeong <bestzenio@gmail.com>
2023-09-14 11:02:26 +0900

    design : appBar ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nomadcoder_flutter_webtoon/widgets/button.dart';
-import 'package:nomadcoder_flutter_webtoon/widgets/currency_card.dart';
+import 'package:nomadcoder_flutter_webtoon/screens/home.screen.dart';
 
 void main() {
   runApp(const App());
@@ -14,137 +13,13 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          // 세로 스크롤
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text("Hey, Berry 🍓",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800,
-                            )),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 64,
-                ),
-                Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white.withOpacity(0.7),
-                  ),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                const Text(
-                  "\$5 184 482",
-                  style: TextStyle(
-                      fontSize: 48,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700),
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: 'Transfer',
-                      bgColor: Color(0xFFF2B33A),
-                      textColor: Colors.black,
-                    ),
-                    Button(
-                      text: 'Request',
-                      bgColor: Color(0xfff1f2123),
-                      textColor: Colors.white,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "Wallets",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Text(
-                      "View All",
-                      style: TextStyle(
-                        color: Colors.white.withOpacity(0.7),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 24,
-                ),
-                const CurrencyCard(
-                  name: "Euro",
-                  code: "EUR",
-                  amount: "6 428",
-                  icon: Icons.euro_rounded,
-                  isInverted: false,
-                  offsetOrder: 0,
-                ),
-                const CurrencyCard(
-                  name: "Dollar",
-                  code: "USD",
-                  amount: "55 622",
-                  icon: Icons.attach_money_rounded,
-                  isInverted: true,
-                  offsetOrder: -20,
-                ),
-                const CurrencyCard(
-                  name: "Rupee",
-                  code: "INR",
-                  amount: "28 981",
-                  icon: Icons.currency_rupee_rounded,
-                  isInverted: false,
-                  offsetOrder: -40,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: HomeScreen(),
+      // home: Scaffold(
+      //   backgroundColor: const Color(0xFF181818),
+      //   body: Container(),
+      //   // 세로 스크롤
+      // ),
     );
   }
 }
